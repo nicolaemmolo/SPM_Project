aacadca05dbe1e91975e04bd5614e3bb0a3eedf2
nicolaemmolo <nicola.emmolo@gmail.com>
2024-09-02 19:48:50 +0200

    partial results FF statis

--- project/test_numa.hs
+++ project/test_numa.hs
@@ -17,7 +17,7 @@ seq_execution() {
 
 # Testing Parallel Static Execution
 par_static_execution() {
-    for T in $(seq 32 $THREADS_STEP $MAX_THREADS); do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
         for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T ps results_UTW_static.csv"
             for rep in $(seq 1 $REPETITIONS); do
@@ -91,10 +91,10 @@ par_dynamic_openmp_execution() {
 # C++ Thread Execution
 #seq_execution
 #par_static_execution
-par_dynamic_execution
+#par_dynamic_execution
 
 # FastFlow Execution
-#par_static_fastflow_execution
+par_static_fastflow_execution
 #par_dynamic_fastflow_execution
 
 # OpenMP Execution
