f9c56cd6b5233231f00ca970f16daf44f653c697
nicolaemmolo <nicola.emmolo@gmail.com>
2024-09-02 13:29:25 +0200

    result for seq and static

---
+++ project/test_cluster.hs
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+THREADS_STEP=2
+MAX_THREADS=40
+REPETITIONS=10
+
+
+# Testing Sequential Execution
+seq_execution() {
+    for N in 128 256 512 1024 2048 4096; do
+        echo "N=$N T=1 s results_UTW_seq.csv"
+        for rep in $(seq 1 $REPETITIONS); do
+            ./UTW $N 1 s results_UTW.csv
+        done
+    done
+}
+
+# Testing Parallel Static Execution
+par_static_execution() {
+    for T in $(seq 1 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
+            echo "N=$N T=$T ps results_UTW_static.csv"
+            for rep in $(seq 1 $REPETITIONS); do
+                ./UTW $N $T ps results_UTW_static.csv
+            done
+        done
+    done
+}
+
+# Testing Parallel Dynamic Execution
+par_dynamic_execution() {
+    for T in $(seq 1 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048; do
+            echo "N=$N T=$T pd results_UTW_dynamic.csv"
+            for rep in $(seq 1 $REPETITIONS); do
+                ./UTW $N $T pd results_UTW_dynamic.csv
+            done
+        done
+    done
+}
+
+# Testing FastFlow Parallel Static Execution
+par_static_fastflow_execution() {
+    for T in $(seq 1 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048; do
+            echo "N=$N T=$T ps results_UTWFF_static.csv"
+            for rep in $(seq 1 $REPETITIONS); do
+                ./UTWFF $N $T ps results_UTWFF_static.csv
+            done
+        done
+    done
+}
+
+# Testing FastFlow Parallel Dynamic Execution
+par_dynamic_fastflow_execution() {
+    for T in $(seq 1 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048; do
+            echo "N=$N T=$T pd results_UTWFF_dynamic.csv"
+            for rep in $(seq 1 $REPETITIONS); do
+                ./UTWFF $N $T pd results_UTWFF_dynamic.csv
+            done
+        done
+    done
+}
+
+# Testing OpenMP Parallel Static Execution
+par_static_openmp_execution() {
+    for T in $(seq 1 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048; do
+            echo "N=$N T=$T ps results_UTWOMP_static.csv"
+            for rep in $(seq 1 $REPETITIONS); do
+                ./UTWOMP $N $T ps results_UTWOMP_static.csv
+            done
+        done
+    done
+}
+
+# Testing OpenMP Parallel Dynamic Execution
+par_dynamic_openmp_execution() {
+    for T in $(seq 1 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048; do
+            echo "N=$N T=$T pd results_UTWOMP_dynamic.csv"
+            for rep in $(seq 1 $REPETITIONS); do
+                ./UTWOMP $N $T pd results_UTWOMP_dynamic.csv
+            done
+        done
+    done
+}
+
+
+# C++ Thread Execution
+#seq_execution
+#par_static_execution
+#par_dynamic_execution
+
+# FastFlow Execution
+#par_static_fastflow_execution
+#par_dynamic_fastflow_execution
+
+# OpenMP Execution
+#par_static_openmp_execution
+#par_dynamic_openmp_execution
--- project/test_numa.hs
+++ project/test_numa.hs
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-THREADS_STEP=4
+THREADS_STEP=2
 MAX_THREADS=40
 REPETITIONS=10
 
@@ -17,8 +17,8 @@ seq_execution() {
 
 # Testing Parallel Static Execution
 par_static_execution() {
-    for T in $(seq 4 $THREADS_STEP $MAX_THREADS); do
-        for N in 128 256 512 1024 2048; do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T ps results_UTW_static.csv"
             for rep in $(seq 1 $REPETITIONS); do
                 ./UTW $N $T ps results_UTW_static.csv
@@ -29,8 +29,8 @@ par_static_execution() {
 
 # Testing Parallel Dynamic Execution
 par_dynamic_execution() {
-    for T in $(seq 30 $THREADS_STEP $MAX_THREADS); do
-        for N in 128 256 512 1024 2048; do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T pd results_UTW_dynamic.csv"
             for rep in $(seq 1 $REPETITIONS); do
                 ./UTW $N $T pd results_UTW_dynamic.csv
@@ -41,8 +41,8 @@ par_dynamic_execution() {
 
 # Testing FastFlow Parallel Static Execution
 par_static_fastflow_execution() {
-    for T in $(seq 4 $THREADS_STEP $MAX_THREADS); do
-        for N in 128 256 512 1024 2048; do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T ps results_UTWFF_static.csv"
             for rep in $(seq 1 $REPETITIONS); do
                 ./UTWFF $N $T ps results_UTWFF_static.csv
@@ -53,8 +53,8 @@ par_static_fastflow_execution() {
 
 # Testing FastFlow Parallel Dynamic Execution
 par_dynamic_fastflow_execution() {
-    for T in $(seq 4 $THREADS_STEP $MAX_THREADS); do
-        for N in 128 256 512 1024 2048; do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T pd results_UTWFF_dynamic.csv"
             for rep in $(seq 1 $REPETITIONS); do
                 ./UTWFF $N $T pd results_UTWFF_dynamic.csv
@@ -65,8 +65,8 @@ par_dynamic_fastflow_execution() {
 
 # Testing OpenMP Parallel Static Execution
 par_static_openmp_execution() {
-    for T in $(seq 4 $THREADS_STEP $MAX_THREADS); do
-        for N in 128 256 512 1024 2048; do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T ps results_UTWOMP_static.csv"
             for rep in $(seq 1 $REPETITIONS); do
                 ./UTWOMP $N $T ps results_UTWOMP_static.csv
@@ -77,8 +77,8 @@ par_static_openmp_execution() {
 
 # Testing OpenMP Parallel Dynamic Execution
 par_dynamic_openmp_execution() {
-    for T in $(seq 4 $THREADS_STEP $MAX_THREADS); do
-        for N in 128 256 512 1024 2048; do
+    for T in 1 $(seq 2 $THREADS_STEP $MAX_THREADS); do
+        for N in 128 256 512 1024 2048 4096; do
             echo "N=$N T=$T pd results_UTWOMP_dynamic.csv"
             for rep in $(seq 1 $REPETITIONS); do
                 ./UTWOMP $N $T pd results_UTWOMP_dynamic.csv
@@ -88,9 +88,15 @@ par_dynamic_openmp_execution() {
 }
 
 
-# Execution
+# C++ Thread Execution
 #seq_execution
-#par_static_execution
+par_static_execution
 #par_dynamic_execution
+
+# FastFlow Execution
 #par_static_fastflow_execution
-par_dynamic_fastflow_execution
+#par_dynamic_fastflow_execution
+
+# OpenMP Execution
+#par_static_openmp_execution
+#par_dynamic_openmp_execution
