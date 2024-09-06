b33d4aada09baffb6b9649c71b8f9fff82e06374
nicolaemmolo <nicola.emmolo@gmail.com>
2024-09-06 11:58:26 +0200

    MPI results and tests (and MakeFIle) + NO OMP

--- project/test_cluster_8nodes.sh
+++ project/test_cluster_8nodes.sh
@@ -18,30 +18,6 @@ mpi_execution() {
     done
 }
 
-mpi_omp_static_execution() {
-    for N in 128 256 512 1024 2048 4096; do
-        for T in 1 2 4 8 16 32; do
-            for rep in $(seq 1 $REPETITIONS); do
-                mpirun ./UTWMPIOMP $N $T ps results_UTWMPI_OMP_static_8.csv 8
-            done
-        done
-    done
-}
-
-mpi_omp_dynamic_execution() {
-    for N in 128 256 512 1024 2048 4096; do
-        for T in 1 2 4 8 16 32; do
-            for rep in $(seq 1 $REPETITIONS); do
-                mpirun ./UTWMPIOMP $N $T pd results_UTWMPI_OMP_dynamic_8.csv 8
-            done
-        done
-    done
-}
-
 
 # MPI execution
 mpi_execution
-
-# MPI + OpenMP execution
-#mpi_omp_static_execution
-#mpi_omp_dynamic_execution
